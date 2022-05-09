6c81d39d28298e4839a5c9b18953640ab6397b30
tusharshr <tushar@mysuperhumanrace.com>
2022-05-09 09:26:38 +0530

    Initial Commit

---
+++ Feedapp/FeedappApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedappApp.swift
+//  Feedapp
+//
+//  Created by Gagandeep on 09/05/22.
+//
+
+import SwiftUI
+
+@main
+struct FeedappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
